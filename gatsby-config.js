35dbce01f1e2fbc168dee6bee8164a29e2fa6e65
Ray Fu <rayfu7@gmail.com>
2019-01-31 15:47:43 -0500

    Add and configure react emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
