a791018066be12a8095ac0b4a6b38d4f0914283c
Ray Fu <rayfu7@gmail.com>
2019-01-31 15:28:22 -0500

    Create navigation bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
+import '../sass/mystyles.scss'
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <button className="button is-primary">Button</button>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
