70877f235e5a41cd715b5e2f3ab0cf16a63c8dd0
Ray Fu <rayfu7@gmail.com>
2019-01-31 22:16:00 -0500

    Merge old work and change file names bc gatsby is bs

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './Header'
+import Header from './header'
 
 const Layout = ({ children }) => (
   <StaticQuery
